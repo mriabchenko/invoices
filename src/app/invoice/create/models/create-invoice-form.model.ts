3b1622c6177ee6ee88009478703d7a82e671d5d8
Nikolay_R <nikolay5011995@gmail.com>
2018-03-25 15:24:35 +0300

    discount field

--- src/app/invoice/create/models/create-invoice-form.model.ts
+++ src/app/invoice/create/models/create-invoice-form.model.ts
@@ -20,5 +20,15 @@ export class CreateInvoiceFormModel {
 
   public addProductToInvoice(product: ProductInterface) {
     console.log('adding to the invoice', product);
+    this.productsForm.push(this.initProductFormGroup(product));
+    console.log(this.productsForm.value);
+  }
+
+  private initProductFormGroup(product: ProductInterface): FormGroup {
+    return this.fb.group({
+      id: product.id,
+      quantity: 1,
+      discount: this.fb.control(0,[Validators.min(0), Validators.max(100)]),
+    });
   }
 }
