b7038d6fa6809cdd14dd08f77c2945809d464631
Nikolay_R <nikolay5011995@gmail.com>
2018-03-26 21:29:19 +0300

    refactoring... (30% done)

--- src/app/invoice/create/create.component.ts
+++ src/app/invoice/create/create.component.ts
@@ -3,66 +3,77 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { CreateInvoiceFormModel } from './models/create-invoice-form.model';
 import { CustomerInterface } from '../interfaces/customer.interface';
 import { RestTransportService } from '../../services/transport/rest-transport.service';
-import { Subscription } from 'rxjs/Subscription';
 import { ProductInterface } from '../interfaces/product.interface';
 import { InvoiceInterface } from '../interfaces/invoice.interface';
 import { InvoiceItemInterface } from '../interfaces/invoice-item.interface';
+import { Store } from '@ngrx/store';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/find';
+import { StoreInterface } from '../../store/interfaces/store.interface';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.sass'],
 })
-export class CreateComponent implements OnInit, OnDestroy {
+export class CreateComponent implements OnDestroy {
   public createInvoiceFormContainer: CreateInvoiceFormModel;
   public newProductPickerForm: FormGroup;
 
-  public customers: CustomerInterface[];
-  public products: ProductInterface[];
+  public customers$: Observable<CustomerInterface[]>;
+  public products$: Observable<ProductInterface[]>;
 
-  public selectedCustomerAddress: string;
-  public invoiceTotal: number;
+  public selectedCustomerAddress$: Observable<string>;
+  public invoiceTotal$: Observable<number>;
 
   private customerSelectSubscription: Subscription;
   private newProductSelectSubscription: Subscription;
   private productsFormSubscription: Subscription;
 
-  public constructor(private fb: FormBuilder, private transport: RestTransportService) {
+  public constructor(private fb: FormBuilder, private transport: RestTransportService, private store: Store<StoreInterface>) {
     this.newProductPickerForm = this.fb.group({
       newProductId: '',
     });
 
+    this.customers$ = this.store.select('state', 'customers');
+    this.products$ = this.store.select('state', 'products');
+
     this.createInvoiceFormContainer = new CreateInvoiceFormModel(this.fb);
-    this.selectedCustomerAddress = 'Customer is not selected';
+    // this.createInvoiceFormContainer
+    //   .customerId
+    //   .valueChanges
+    //   .map((selectedCustomerId: number) => {
+    //     console.log(selectedCustomerId);
+    //     return
+    //
+    this.selectedCustomerAddress$ = this.customers$
+      .map((customers: CustomerInterface[]) => customers.find((customer: CustomerInterface) => customer.id === 1).address);
+
+    // this.newProductSelectSubscription = this.newProductPickerForm.get('newProductId').valueChanges.subscribe((newProductId: number) => {
+    //   const selectedProduct = this.products.find((product: ProductInterface) => product.id === +newProductId);
+    //   this.createInvoiceFormContainer.addProductToInvoice(selectedProduct);
+    // });
+    //
+    // this.productsFormSubscription = this.createInvoiceFormContainer.productsForm.valueChanges.subscribe((products: InvoiceItemInterface[]) => {
+    //   if (this.createInvoiceFormContainer.productsForm.valid) {
+    //     this.invoiceTotal = this.createInvoiceFormContainer.calcInvoiceTotal(products);
+    //     const invoice: InvoiceInterface = {
+    //       id: 1,
+    //       customer_id: +this.createInvoiceFormContainer.customerId.value,
+    //       discount: 0,
+    //       total: this.invoiceTotal,
+    //     };
+    //     this.transport.createInvoice(invoice);
+    //   }
+    // });
   }
 
-  public ngOnInit(): void {
-    this.customerSelectSubscription = this.createInvoiceFormContainer.customerId.valueChanges.subscribe((selectedCustomerId: number) => {
-      this.selectedCustomerAddress = this.customers.find((customer: CustomerInterface) => customer.id === +selectedCustomerId).address;
-    });
-
-    this.newProductSelectSubscription = this.newProductPickerForm.get('newProductId').valueChanges.subscribe((newProductId: number) => {
-      const selectedProduct = this.products.find((product: ProductInterface) => product.id === +newProductId);
-      this.createInvoiceFormContainer.addProductToInvoice(selectedProduct);
-    });
-
-    this.productsFormSubscription = this.createInvoiceFormContainer.productsForm.valueChanges.subscribe((products: InvoiceItemInterface[]) => {
-      if (this.createInvoiceFormContainer.productsForm.valid) {
-        this.invoiceTotal = this.createInvoiceFormContainer.calcInvoiceTotal(products);
-        const invoice: InvoiceInterface = {
-          id: 1,
-          customer_id: +this.createInvoiceFormContainer.customerId.value,
-          discount: 0,
-          total: this.invoiceTotal,
-        };
-        this.transport.createInvoice(invoice);
-      }
-    });
-  }
 
   public ngOnDestroy(): void {
-    this.customerSelectSubscription.unsubscribe();
-    this.newProductSelectSubscription.unsubscribe();
-    this.productsFormSubscription.unsubscribe();
+    // this.customerSelectSubscription.unsubscribe();
+    // this.newProductSelectSubscription.unsubscribe();
+    // this.productsFormSubscription.unsubscribe();
   }
 }
